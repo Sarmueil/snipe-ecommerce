956534d86394c0ef409d0fd28ef973dc58f03914
Sarmueil <adedoyin.adebisi@decagon.dev>
2021-11-04 00:17:09 +0100

    implmenting-homepage-basic-routing

--- snipe-frontend/src/Components/Homepage/Homepage.jsx
+++ snipe-frontend/src/Components/Homepage/Homepage.jsx
@@ -1,30 +1,30 @@
-// import React, {useState, useEffect}from 'react'
-// import axios from 'axios';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Carousel from 'react-elastic-carousel'
-// import Header from '../Header/Header';
-// import Hero from '../Hero/Hero';
-// import Carol from '../Carol/Carol';
-// import Mainsection from '../Mainsection/Mainsection'
-// import HeroHeader from '../HeroHeader/HeroHeader';
-// import Category from '../Catergory/Category';
-// import CatergoryHeader from '../CatergoryHeader/CatergoryHeader'; 
-// import XclusiveHeader from '../XclusiveHeader/XclusiveHeader'
-// import Section from '../Section/Section';
-// import Featured from '../Featured/Featured';
-// import Footer from '../Footer/Footer'
+import React, {useState, useEffect}from 'react'
+import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import Carousel from 'react-elastic-carousel'
+import Header from '../Header/Header';
+import Hero from '../Hero/Hero';
+import Carol from '../Carol/Carol';
+import Mainsection from '../Mainsection/Mainsection'
+import HeroHeader from '../HeroHeader/HeroHeader';
+import Category from '../Catergory/Category';
+import CatergoryHeader from '../CatergoryHeader/CatergoryHeader'; 
+import XclusiveHeader from '../XclusiveHeader/XclusiveHeader'
+import Section from '../Section/Section';
+import Featured from '../Featured/Featured';
+import Footer from '../Footer/Footer'
 
-// // import newproducts from '../../new'; 
+// import newproducts from '../../new'; 
  
-// const useStlyes = makeStyles({
-//     Carousel:{
-//         marginTop:'50px',
-//     },
-//     Homepage:{
-//        width:'100%',   
-//        position:'relative' 
-//     }
-// });
+const useStlyes = makeStyles({
+    Carousel:{
+        marginTop:'50px',
+    },
+    Homepage:{
+       width:'100%',   
+       position:'relative' 
+    }
+});
   
 // const breakPoints = [
 //     {width:1, itemsToShow: 1},
@@ -34,7 +34,7 @@
 // ]
 
 
-// const Homepage = () => {
+const Homepage = () => {
 //     const [ newProducts, setNewProducts ] = useState([]);
 //    useEffect(()=>{
 //        const fetchNewProducts =async()=>{
@@ -51,34 +51,34 @@
 //        }
 //        fetchRelated()
 //    },[])
-//     const classes = useStlyes()   
-//     return (
-//         <div className={classes.Homepage}>     
-//             <Header />
-//             <Hero /> 
-//              <HeroHeader />    
-//         <Carousel className={classes.Carousel} breakPoints={breakPoints} enableAutoPlay={true} autoPlaySpeed={4000}>
-//                   {newProducts.map((product)=>(
-//                     <Carol  product={product}/>
-//                   ))} 
-//              </Carousel>
-//            <Mainsection /> 
-//            <CatergoryHeader />
-//           <Category />
-//           <XclusiveHeader />
-//           <Carousel className={classes.Carousel} breakPoints={breakPoints} enableAutoPlay={true} autoPlaySpeed={4000}>
-//                   {relatedProducts.map((product)=>(
-//                     <Carol  product={product}/>
-//                   ))} 
-//              </Carousel>
-//              <Featured /> 
-//             <Section />
-//             <Footer />
-//         </div>
-//     )
-// }
+    const classes = useStlyes()   
+    return (
+        <div className={classes.Homepage}>     
+            <Header />
+            <Hero /> 
+             <HeroHeader />    
+        <Carousel className={classes.Carousel} breakPoints={breakPoints} enableAutoPlay={true} autoPlaySpeed={4000}>
+                  {newProducts.map((product)=>(
+                    <Carol  product={product}/>
+                  ))} 
+             </Carousel>
+           <Mainsection /> 
+           <CatergoryHeader />
+          <Category />
+          <XclusiveHeader />
+          <Carousel className={classes.Carousel} breakPoints={breakPoints} enableAutoPlay={true} autoPlaySpeed={4000}>
+                  {relatedProducts.map((product)=>(
+                    <Carol  product={product}/>
+                  ))} 
+             </Carousel>
+             <Featured /> 
+            <Section />
+            <Footer />
+        </div>
+    )
+}
 
-// export default Homepage
+export default Homepage
 
 
 
