ebb0520ce73cd218495a26b067ea28b41296f82b
Sarmueil <adedoyin.adebisi@decagon.dev>
2021-11-04 00:22:10 +0100

    hero-component-homepage

--- snipe-frontend/src/Components/Hero/Hero.jsx
+++ snipe-frontend/src/Components/Hero/Hero.jsx
@@ -1,57 +1,57 @@
-// import React from 'react'
-// import { makeStyles } from '@material-ui/core/styles';
-// import './Hero.css'
-// import HeroA from '../../Images/HeroA.svg'
-// import HeroB from '../../Images/HeroB.svg'
-// import HeroC from '../../Images/HeroC.svg'
-// import HeroD from '../../Images/HeroD.svg'
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import './Hero.css'
+import HeroA from '../../Images/HeroA.svg'
+import HeroB from '../../Images/HeroB.svg'
+import HeroC from '../../Images/HeroC.svg'
+import HeroD from '../../Images/HeroD.svg'
 
-// const useStlyes = makeStyles({
-//     hero:{
-//         width: '100%',
-//         background:'black'
-//     },
-// });
-// const Hero = () => {
-//     const classes = useStlyes()
-//     return (
-//         <div className={classes.hero}>
-//              <div className='hero_slider'>
-//                     <div className='hero_items '>
-//                        <img src={HeroA} alt='hero-image'/>
-//                       <div className='hero_text'>
-//                           <h5>snipe it love more</h5>
-//                           <p>Explore a curated selection of footwaer brands for your own comfort.</p>
-//                       </div>
-//                     </div>
-//                     <div className='hero_items '>
-//                        <img src={HeroB} alt='hero-image'/>
-//                       <div className='hero_text'>
-//                           <h5>go run go steady</h5>
-//                           <p>Hit the road all day long, your move your way.</p>
-//                       </div>
-//                     </div>
-//                     <div className='hero_items '>
-//                        <img src={HeroC} alt='hero-image'/>
-//                       <div className='hero_text'>
-//                           <h5>form and function</h5>
-//                           <p>Fit and bold and comfortable footwears,let us encourage your endeavors.</p>
-//                       </div>
-//                     </div>
-//                     <div className='hero_items '>
-//                        <img src={HeroD} alt='hero-image'/>
-//                       <div className='hero_text'>
-//                           <h5>The fashion way</h5>
-//                           <p>Shop our new and exclusive footwear brands to complement your clothing style</p>
-//                       </div>
-//                 </div>
-//             </div>
-//                <div className='hero_btn'>
-//                    <div className='explore_btn'>Explore</div>
-//                    <div className='shop_btn'>Shop</div> 
-//                </div>
-//         </div>
-//     )
-// }
+const useStlyes = makeStyles({
+    hero:{
+        width: '100%',
+        background:'black'
+    },
+});
+const Hero = () => {
+    const classes = useStlyes()
+    return (
+        <div className={classes.hero}>
+             <div className='hero_slider'>
+                    <div className='hero_items '>
+                       <img src={HeroA} alt='hero-image'/>
+                      <div className='hero_text'>
+                          <h5>snipe it love more</h5>
+                          <p>Explore a curated selection of footwaer brands for your own comfort.</p>
+                      </div>
+                    </div>
+                    <div className='hero_items '>
+                       <img src={HeroB} alt='hero-image'/>
+                      <div className='hero_text'>
+                          <h5>go run go steady</h5>
+                          <p>Hit the road all day long, your move your way.</p>
+                      </div>
+                    </div>
+                    <div className='hero_items '>
+                       <img src={HeroC} alt='hero-image'/>
+                      <div className='hero_text'>
+                          <h5>form and function</h5>
+                          <p>Fit and bold and comfortable footwears,let us encourage your endeavors.</p>
+                      </div>
+                    </div>
+                    <div className='hero_items '>
+                       <img src={HeroD} alt='hero-image'/>
+                      <div className='hero_text'>
+                          <h5>The fashion way</h5>
+                          <p>Shop our new and exclusive footwear brands to complement your clothing style</p>
+                      </div>
+                </div>
+            </div>
+               <div className='hero_btn'>
+                   <div className='explore_btn'>Explore</div>
+                   <div className='shop_btn'>Shop</div> 
+               </div>
+        </div>
+    )
+}
 
-// export default Hero
+export default Hero
