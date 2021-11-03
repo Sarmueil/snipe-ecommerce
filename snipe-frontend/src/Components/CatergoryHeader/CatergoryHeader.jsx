0c6ec46b84e09de1a48936a485b2c17e6608aabe
Sarmueil <adedoyin.adebisi@decagon.dev>
2021-11-04 00:29:36 +0100

    catergory-header-component

--- snipe-frontend/src/Components/CatergoryHeader/CatergoryHeader.jsx
+++ snipe-frontend/src/Components/CatergoryHeader/CatergoryHeader.jsx
@@ -1,54 +1,54 @@
-// import React from 'react'
-// import '@fontsource/poppins';
-// import { makeStyles } from '@material-ui/core/styles';
+import React from 'react'
+import '@fontsource/poppins';
+import { makeStyles } from '@material-ui/core/styles';
 
-// const useStlyes = makeStyles({
-//     HeroHeader:{
-//        display:'flex',
-//        justifyContent:'space-between',
-//        alignItems:'center',
-//        marginTop:'20px',
-//     },
-//     heroTrend:{
-//         marginTop:'3%',
-//        marginLeft:'6%',
-//         fontSize:'40px',
-//         color:'black',
-//         fontFamily:'poppins',
-//        fontWeight:'300',
-//        letterSpacing:'1px'
-//     },
-//     trendBtn:{
-//         background:'#E5E5E5',
-//         color:'black',
-//         letterSpacing:'1px',
-//         padding:'12px',
-//         fontFamily:'poppins',
-//         fontSize:'20px',
-//         marginRight:'5%',
-//         marginTop:'3%',
-//         borderRadius:'25px',
-//         cursor:'pointer',
+const useStlyes = makeStyles({
+    HeroHeader:{
+       display:'flex',
+       justifyContent:'space-between',
+       alignItems:'center',
+       marginTop:'20px',
+    },
+    heroTrend:{
+        marginTop:'3%',
+       marginLeft:'6%',
+        fontSize:'40px',
+        color:'black',
+        fontFamily:'poppins',
+       fontWeight:'300',
+       letterSpacing:'1px'
+    },
+    trendBtn:{
+        background:'#E5E5E5',
+        color:'black',
+        letterSpacing:'1px',
+        padding:'12px',
+        fontFamily:'poppins',
+        fontSize:'20px',
+        marginRight:'5%',
+        marginTop:'3%',
+        borderRadius:'25px',
+        cursor:'pointer',
 
-//         '&:hover': {
-//             background:'black',
-//             color:'#E5E5E5',
-//             transition: 'all 0.3s linear',
-//          },
-//     }
-// });
-// const CatergoryHeader = () => {
-//     const classes = useStlyes()
-//     return (
-//         <div className={classes.HeroHeader}>
-//             <div className={classes.heroTrend}>
-//               Snipe More
-//             </div>
-//             <div className={classes.trendBtn}>
-//                Explore
-//             </div>
-//         </div>
-//     )
-// }
+        '&:hover': {
+            background:'black',
+            color:'#E5E5E5',
+            transition: 'all 0.3s linear',
+         },
+    }
+});
+const CatergoryHeader = () => {
+    const classes = useStlyes()
+    return (
+        <div className={classes.HeroHeader}>
+            <div className={classes.heroTrend}>
+              Snipe More
+            </div>
+            <div className={classes.trendBtn}>
+               Explore
+            </div>
+        </div>
+    )
+}
 
-// export default CatergoryHeader
+export default CatergoryHeader
