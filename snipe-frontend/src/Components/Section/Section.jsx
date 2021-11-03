16713360cd1ae2951cdbb8aac84340f0620fa043
Sarmueil <adedoyin.adebisi@decagon.dev>
2021-11-04 00:39:40 +0100

    homepage-section-component

--- snipe-frontend/src/Components/Section/Section.jsx
+++ snipe-frontend/src/Components/Section/Section.jsx
@@ -1,40 +1,40 @@
-// import React from 'react'
-// import '@fontsource/poppins';
-// import './Section.css'
-// import { makeStyles } from '@material-ui/core/styles';
+import React from 'react'
+import '@fontsource/poppins';
+import './Section.css'
+import { makeStyles } from '@material-ui/core/styles';
 
-// const useStlyes = makeStyles({
-//     section:{
-//         background:'#E5E5E5',
-//         textAlign:'center',
-//         color:'black',
-//         marginTop:'4%',
-//         padding:'100px 0'
-//     },
-//     section_id:{
-//         fontSize:'60px',
-//         fontWeight:'800',
-//         fontFamily:'poppins',
-//         letterSpacing:'2px', 
-//     },
-//     section_idb:{
-//         fontSize:'18px',
-//         fontFamily:'poppins',
-//         letterSpacing:'1px',
-//     }
-// });
-// const Section = () => {
-//     const classes = useStlyes()
-//     return (
-//         <div className={classes.section}>
-//         <h2 className={classes.section_id}>SNIPE AHEAD</h2>
-//         <p className={classes.section_idb}>A rich experience, exlporing our diverse footwear brands.</p>
-//         <div className='section_btn'>
-//               <div className='exploresec_btn'>Explore</div>
-//               <div className='shopsec_btn'>Shop</div>
-//            </div>
-//       </div>
-//     )
-// }
+const useStlyes = makeStyles({
+    section:{
+        background:'#E5E5E5',
+        textAlign:'center',
+        color:'black',
+        marginTop:'4%',
+        padding:'100px 0'
+    },
+    section_id:{
+        fontSize:'60px',
+        fontWeight:'800',
+        fontFamily:'poppins',
+        letterSpacing:'2px', 
+    },
+    section_idb:{
+        fontSize:'18px',
+        fontFamily:'poppins',
+        letterSpacing:'1px',
+    }
+});
+const Section = () => {
+    const classes = useStlyes()
+    return (
+        <div className={classes.section}>
+        <h2 className={classes.section_id}>SNIPE AHEAD</h2>
+        <p className={classes.section_idb}>A rich experience, exlporing our diverse footwear brands.</p>
+        <div className='section_btn'>
+              <div className='exploresec_btn'>Explore</div>
+              <div className='shopsec_btn'>Shop</div>
+           </div>
+      </div>
+    )
+}
 
-// export default Section
+export default Section
