683edbd6876277138f7e3cb6090290b65dabc4dc
Sarmueil <adedoyin.adebisi@decagon.dev>
2021-11-04 01:33:49 +0100

    kids-products-main-page

--- snipe-frontend/src/Components/KidsProductPage/KidsProductPage.jsx
+++ snipe-frontend/src/Components/KidsProductPage/KidsProductPage.jsx
@@ -1,22 +1,22 @@
-// import React, {useState, useEffect} from 'react';
-// import axios from 'axios';
-// import styled from 'styled-components'; 
-// import { makeStyles } from '@material-ui/core/styles';
-// import Carousel from 'react-elastic-carousel'
-// import KidsHeader from '../KidsHeader/KidsHeader';
-// import Carol from '../Carol/Carol';
-// import Header from '../Header/Header';
-// import XclusiveHeader from '../XclusiveHeader/XclusiveHeader'
-// import NewProducts from '../NewProducts/NewProducts'; 
-// import Section from '../Section/Section';
-// import KidsProductHero from '../KidsProductHero/KidsProductHero';
-// import Footer from '../Footer/Footer'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import styled from 'styled-components'; 
+import { makeStyles } from '@material-ui/core/styles';
+import Carousel from 'react-elastic-carousel'
+import KidsHeader from '../KidsHeader/KidsHeader';
+import Carol from '../Carol/Carol';
+import Header from '../Header/Header';
+import XclusiveHeader from '../XclusiveHeader/XclusiveHeader'
+import NewProducts from '../NewProducts/NewProducts'; 
+import Section from '../Section/Section';
+import KidsProductHero from '../KidsProductHero/KidsProductHero';
+import Footer from '../Footer/Footer'
 
-// const useStlyes = makeStyles({
-//     Carousel:{ 
-//         marginTop:'50px',
-//     } 
-// });
+const useStlyes = makeStyles({
+    Carousel:{ 
+        marginTop:'50px',
+    } 
+});
 
 // const breakPoints = [
 //     {width:1, itemsToShow: 1},
@@ -25,7 +25,7 @@
 //     {width:1200, itemsToShow: 3}
 // ]
 
-// const KidsProductPage = () => {
+const KidsProductPage = () => {
 //     const [ products, setProducts ] = useState([]);
 //     useEffect(()=>{
 //         const fetchProducts = async()=>{
@@ -58,90 +58,90 @@
 //         }
 //         fetchRelated()
 //     },[])
-//     const classes = useStlyes()
-//     return (
-//         <div className='newProductPage'>
-//           <Header/> 
-//           <KidsHeader  />
-//           <FilterContainer>
-//              <Filter><FilterText>Filters</FilterText>
-//                 <Select>
-//                     <Option disabled selected>Categories</Option>
-//                     <Option>Sneakers</Option> 
-//                     <Option>Leather shoes</Option>
-//                     <Option>Canvas</Option>
-//                     <Option>Sport shoes</Option>
-//                 </Select>
-//              </Filter>
-//              <Filter><FilterText>Sort By</FilterText>
-//              <Select>
-//                     <Option disabled selected>Size</Option>
-//                     <Option>S</Option>
-//                     <Option>M</Option>
-//                     <Option>L</Option>
-//                     <Option>XL</Option>
-//                 </Select>
-//              </Filter>
-//           </FilterContainer>
-//            <Container>
-//                {products.map((item)=>(
-//                   <NewProducts item={item} key={item._id}/>   
-//                ))} 
-//            </Container>
-//           <Carousel className={classes.Carousel} breakPoints={breakPoints} enableAutoPlay={true} autoPlaySpeed={4000}>
-//                   {newProducts.map((product)=>(
-//                     <Carol  product={product}/>
-//                   ))} 
-//              </Carousel>
-//              <Container> 
-//                {xclusive.map((item)=>(
-//                   <NewProducts item={item} key={item._id}/>    
-//                ))} 
-//            </Container> 
-//            <KidsProductHero />
-//            <XclusiveHeader />
-//            <Carousel className={classes.Carousel} breakPoints={breakPoints} enableAutoPlay={true} autoPlaySpeed={4000}>
-//                   {relatedProducts.map((product)=>(
-//                     <Carol product={product}/>
-//                   ))} 
-//              </Carousel>
-//              <Section />
-//             <Footer />
-//         </div>
-//     )
-// }
+    const classes = useStlyes()
+    return (
+        <div className='newProductPage'>
+          <Header/> 
+          <KidsHeader  />
+          <FilterContainer>
+             <Filter><FilterText>Filters</FilterText>
+                <Select>
+                    <Option disabled selected>Categories</Option>
+                    <Option>Sneakers</Option> 
+                    <Option>Leather shoes</Option>
+                    <Option>Canvas</Option>
+                    <Option>Sport shoes</Option>
+                </Select>
+             </Filter>
+             <Filter><FilterText>Sort By</FilterText>
+             <Select>
+                    <Option disabled selected>Size</Option>
+                    <Option>S</Option>
+                    <Option>M</Option>
+                    <Option>L</Option>
+                    <Option>XL</Option>
+                </Select>
+             </Filter>
+          </FilterContainer>
+           <Container>
+               {products.map((item)=>(
+                  <NewProducts item={item} key={item._id}/>   
+               ))} 
+           </Container>
+          <Carousel className={classes.Carousel} breakPoints={breakPoints} enableAutoPlay={true} autoPlaySpeed={4000}>
+                  {newProducts.map((product)=>(
+                    <Carol  product={product}/>
+                  ))} 
+             </Carousel>
+             <Container> 
+               {xclusive.map((item)=>(
+                  <NewProducts item={item} key={item._id}/>    
+               ))} 
+           </Container> 
+           <KidsProductHero />
+           <XclusiveHeader />
+           <Carousel className={classes.Carousel} breakPoints={breakPoints} enableAutoPlay={true} autoPlaySpeed={4000}>
+                  {relatedProducts.map((product)=>(
+                    <Carol product={product}/>
+                  ))} 
+             </Carousel>
+             <Section />
+            <Footer />
+        </div>
+    )
+}
 
-// export default KidsProductPage
+export default KidsProductPage
 
-// const Container = styled.div `
-//    padding:20px;
+const Container = styled.div `
+   padding:20px;
+   display:flex;
+   flex-wrap:wrap;
+`;
+const FilterContainer = styled.div `
+   display:flex;
+   justify-content:flex-end;
+`;
+const Filter = styled.div `
 //    display:flex;
-//    flex-wrap:wrap;
-// `;
-// const FilterContainer = styled.div `
-//    display:flex;
-//    justify-content:flex-end;
-// `;
-// const Filter = styled.div `
-// //    display:flex;
-//    margin:20px;
-// `;
-// const FilterText = styled.span `
-//    display:flex;
-//    align-items:center;
-//    font-size:17px;
-//     color:black;
-//     font-family:poppins;
-//     font-weight:400;
-//     letter-spacing:1px;
-//     cursor:pointer;
-//     margin-right: 20px;
-// `;
-// const Select = styled.select `
-// padding: 10px;
-// margin-right: 20px;
-// `; 
-// const Option = styled.option `
-//     font-size:15px;
+   margin:20px;
+`;
+const FilterText = styled.span `
+   display:flex;
+   align-items:center;
+   font-size:17px;
+    color:black;
+    font-family:poppins;
+    font-weight:400;
+    letter-spacing:1px;
+    cursor:pointer;
+    margin-right: 20px;
+`;
+const Select = styled.select `
+padding: 10px;
+margin-right: 20px;
+`; 
+const Option = styled.option `
+    font-size:15px;
      
-// `;
+`;
