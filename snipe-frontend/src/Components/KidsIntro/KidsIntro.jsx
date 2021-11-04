b9b28c1389fe6c82f9d64d865458a5513fdc5704
Sarmueil <adedoyin.adebisi@decagon.dev>
2021-11-04 01:25:21 +0100

    react-elastic-carousel-kids-products-landing-page

--- snipe-frontend/src/Components/KidsIntro/KidsIntro.jsx
+++ snipe-frontend/src/Components/KidsIntro/KidsIntro.jsx
@@ -22,12 +22,12 @@ const useStlyes = makeStyles({
     }
 });
  
-// const breakPoints = [
-//     {width:1, itemsToShow: 1},
-//     {width:550, itemsToShow: 2}, 
-//     {width:768, itemsToShow: 3},
-//     {width:1200, itemsToShow: 3}
-// ]
+const breakPoints = [
+    {width:1, itemsToShow: 1},
+    {width:550, itemsToShow: 2}, 
+    {width:768, itemsToShow: 3},
+    {width:1200, itemsToShow: 3}
+]
 
 const KidsIntro = () => {
 //   const [newProducts, setNewProducts]= useState([]);
